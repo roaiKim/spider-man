30a455c9b7a5b5ddab79f01bbb76f292dcc9534f
roaiKim <shcroai@gmail.com>
2021-08-10 00:14:46 +0800

    update item

--- src/module/navigation/component/tabs.jsx
+++ src/module/navigation/component/tabs.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import SVG from "./tab-svg";
 
-export default function tabs() {
+export default function TabComponent() {
     return (
         <div className="surface">
             <div className="chrome-tabs">
                 <div className="chrome-tabs-content">
-                    <div className="chrome-tab" active="trsue">
-                        <div className="chrome-tab-dividers" />
-                        <div className="chrome-tab-background">
-                            <SVG />
-                        </div>
-                        <div className="chrome-tab-content">
-                            <div className="chrome-tab-favicon chrome-google" />
-                            <div className="chrome-tab-title">Google</div>
-                            <div className="chrome-tab-drag-handle" />
-                            <div className="chrome-tab-close" />
-                        </div>
-                    </div>
+                    <ChromeTab tabs={[1, 2, 3]} active={2} />
                 </div>
                 <div className="chrome-tabs-bottom-bar" />
             </div>
         </div>
     );
+}
+
+function ChromeTab({ tabs, active }) {
+
+    return tabs.map((item) => (
+        <div key={item} className="chrome-tab" {...(active === item ? { active: "true" } : {})}>
+            <div className="chrome-tab-dividers" />
+            <div className="chrome-tab-background">
+                <SVG />
+            </div>
+            <div className="chrome-tab-content">
+                <div className="chrome-tab-favicon chrome-google" />
+                <div className="chrome-tab-title">Google</div>
+                <div className="chrome-tab-drag-handle" />
+                <div className="chrome-tab-close" />
+            </div>
+        </div>
+    ));
 }
